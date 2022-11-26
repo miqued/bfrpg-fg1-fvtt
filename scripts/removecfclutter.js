3e14a02e2df7c7b53f686c8e643bdbf39afddbb3
miqued <20347557+miqued@users.noreply.github.com>
2022-11-26 09:41:33 -0500

    fixed Compendium Folders id to match its actual id

--- scripts/removecfclutter.js
+++ scripts/removecfclutter.js
@@ -1,6 +1,6 @@
 Hooks.on('renderCompendium', (doc, html, context) => {
     // Skip entirely if it's someone else's compendium
     if (! doc.metadata.packageName === 'bfrpg-fieldguide-volume1') return
-    if (game.modules.get('compendium-folder')?.active) return
+    if (game.modules.get('compendium-folders')?.active) return
     html.find('.document-name a:contains("#[CF_")').parent().parent().remove()
 })
