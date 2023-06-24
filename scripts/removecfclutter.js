31ac7db0ad57e69c63eaa3bd2cbaa8fc03b94970
miqued <20347557+miqued@users.noreply.github.com>
2023-06-23 21:31:24 -0400

    remove unnecessary script

--- scripts/removecfclutter.js
+++
@@ -1,6 +0,0 @@
-Hooks.on('renderCompendium', (doc, html, context) => {
-    // Skip entirely if it's someone else's compendium
-    if (! doc.metadata.packageName === 'bfrpg-fg1-fvtt') return
-    if (game.modules.get('compendium-folders')?.active) return
-    html.find('.document-name a:contains("#[CF_")').parent().parent().remove()
-})
